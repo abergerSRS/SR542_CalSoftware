0e3370bbe9bc2a33c8e514a8521433448bb25d24
Andy Berger <aberger@corp.thinksrs.com>
2020-09-14 18:40:36 -0700

    fix divide-by-zero bug in resample function in torqueCorrection_v2.py

--- torqueCorrection_v2.py
+++ torqueCorrection_v2.py
@@ -14,8 +14,9 @@ torque non-uniformity
 """
 
 import numpy as np
+from numpy import pi
+
 import matplotlib.pyplot as plt
-from numpy import sqrt, pi, exp, linspace, random
 import os
 
 # custom modules
@@ -23,21 +24,26 @@ import fileWriter
 
 plt.close('all')
 
-#data with angle correction, for torque calibration:
-filename = r'D:\Documents\MCUXpressoIDE_10.1.0_589\workspace\SR544\tools\phaseAndSpeed_constQ_2.6Hz.txt'
+file_dir = os.path.abspath(r"C:\Users\aberger\Documents\Projects\SR542\Firmware\SR544\tools")
 
-data = np.loadtxt(filename, delimiter=' ', usecols=[0,1,2,3], skiprows=0)
+filename = r"torqueCal_constQ_3Hz_10-100blade_CW.txt"
+#filename = r"torqueCal_constQ_3Hz_10-100blade_CW_verify.txt"
+#filename = r"torqueCal_constQ_3Hz_noBlade_CW.txt"
 
-N_enc = 100
+full_path = os.path.join(file_dir, filename)
+
+data = np.loadtxt(full_path, delimiter=' ', usecols=[0,1,2,3], skiprows=0)
+
+N_enc = 400
 f_FTM = 60e6
 FTM_MOD = 4096
-dt = FTM_MOD*64/f_FTM 
-#data is being sampled at ~4 ms
+dt = FTM_MOD*128/f_FTM 
+#data is being sampled at ~8.7 ms
 #based on an FTM3PERIOD_S = 6.8266667e-5
-#and a samping prescale factor of 64                    
+#and a samping prescale factor of 128                    
 
 N = len(data[:,0]) 
-time = linspace(0,(N-1)*dt,N)
+time = np.linspace(0,(N-1)*dt,N)
 
 rotorAngle_rawCount = data[:,0]
 shaftInputCapture = data[:,1]
@@ -45,23 +51,25 @@ numEdges = data[:,2]
 rawPhase = 2*pi*data[:,3]/(2**32) #in rad
 uwPhase = np.unwrap(rawPhase) #in rad
 
-#moment = 1.21e-5 #kg*m^2, from I_zz Fusion 360 model of encoder disc
-moment = 1.7e-5 #kg*m^2, from impulse measurements
-#torqueConst = 5.55e-3 #N*m/A, from Nuelectronics motor spec sheet
-torqueConst = 7.48e-3 #N*m/A, from Elinco motor spec sheet
 
-def resample(x,y,numPts):
+moment = 1.7e-5 #kg*m^2, from impulse measurements
+#UWE 10-100 Blade (Stainless Steel)
+#moment = 0.648*1.829e-5 #oz*in^2 from I_zz Fusion 360 model, to kg*m^2
+torqueConst = 5.55e-3 #N*m/A, from Nuelectronics motor spec sheet
+#torqueConst = 7.48e-3 #N*m/A, from Elinco motor spec sheet
+
+def resample(x, y, numPts):
     """
-    resample (x,y) such that len(x_out) = len(x)/dec
+    resample (x,y) such that len(y_resamp) = numPts
     
     uses a two-pass method to calculate the average of y at the new bin spacing,
     followed by the standard deviation of y_resamp based on the scatter 
     in the original y data
     """
     
-    dx = max(x)/(numPts+1)
+    dx = max(x)/(numPts+1) # new bin spacing
     
-    x_resamp = linspace(0,max(x)-dx,numPts)
+    x_resamp = np.linspace(0, max(x)-dx, numPts) # new bins
     
     
     y_resamp = np.zeros(numPts)
@@ -70,17 +78,21 @@ def resample(x,y,numPts):
     i = 0 #iterator over x_resamp values
     for x_n in x_resamp:
         
-        n = 0
-        total = 0
-        j = 0 #iterator over y values
-        for value in x:
+        n = 0       # total number of y-values in the i-th bin
+        total = 0   # sum of y-values in the i-th bin
+        j = 0       # iterator over y values
+        for value in x: # iterate over the entire array of original x values
             if(x_n - dx/2 <= value < x_n + dx/2):
+                # and if x is in this bin, the y value at this index should be
+                # counted in this bin
                 total += y[j]
                 n += 1
             j += 1
         
-        y_resamp[i] = total/n
-        #print(y_resamp[i])
+        if(n != 0):
+            y_resamp[i] = total/n
+        else: 
+            y_resamp[i] = 0
         
         #again iterate over y values to calculate standard deviation
         sum_sq = 0
@@ -90,14 +102,16 @@ def resample(x,y,numPts):
                 sum_sq += (y[j] - y_resamp[i])**2
             j += 1
             
-        y_stdev[i] = np.sqrt(sum_sq/(n - 1))
-        #print(res_stdev[i])
+        if(n != 0):
+            y_stdev[i] = np.sqrt(sum_sq/(n))
+        else:
+            y_stdev[i] = 0
         
         i += 1
         
     return [x_resamp,y_resamp,y_stdev]
 
-def smooth(x,window_len=11,window='hanning'):
+def smooth(x, window_len=11, window='hanning'):
     """smooth the data using a window with requested size.
     
     from: https://scipy-cookbook.readthedocs.io/items/SignalSmooth.html
@@ -118,7 +132,7 @@ def smooth(x,window_len=11,window='hanning'):
         
     example:
 
-    t=linspace(-2,2,0.1)
+    t=np.linspace(-2,2,0.1)
     x=sin(t)+randn(len(t))*0.1
     y=smooth(x)
     
@@ -161,61 +175,65 @@ def smooth(x,window_len=11,window='hanning'):
     return y[int(window_len/2):-int(window_len/2)]
 
 
-# differentiate unwrapped angle twice to calculate acceleration
+# Differentiate unwrapped angle twice to calculate acceleration
 dAngle_dt = np.gradient(uwPhase, dt)
-# calculate smoothed speed
-#omega_smth = smooth(omega, window_len = 17, window = 'hanning')
-
 d2Angle_dt2 = np.gradient(dAngle_dt, dt)
 
+# Also calculate speed using num of FTM counts (shaftInputCapture) and 
+# number of Captured Edges
 deltaT_sec = shaftInputCapture/(f_FTM*numEdges)
 inputCapSpeed = 1/(N_enc*deltaT_sec)
+inputCapAccel = np.gradient(inputCapSpeed*2*np.pi, dt)
 
 fig1, ax1 = plt.subplots()
 ax1.plot(time, dAngle_dt)
 ax1.plot(time, inputCapSpeed*2*np.pi)
 ax1.set_xlabel('time (s)')
 ax1.set_ylabel('shaft speed (rad/s)')
-ax1.legend((r'$\theta(t)$', r'$\Delta_{FTM}(t)$'))
-
-inputCapAccel = np.gradient(inputCapSpeed*2*np.pi, dt)
+ax1.legend((r'$\partial (\mathrm{motor.phase})/\partial t$', r'from $\Delta_{FTM}$'))
+ax1.set_title('Shaft speed vs time')
 
 fig2, ax2 = plt.subplots()
 ax2.plot(time, d2Angle_dt2)
 ax2.plot(time, inputCapAccel)
 ax2.set_xlabel('time (s)')
 ax2.set_ylabel('accel rad/s^2')
-ax2.legend((r'$\theta(t)$', r'$\Delta_{FTM}(t)$'))
+ax2.legend((r'$\partial^2 (\mathrm{motor.phase})/\partial t^2$', r'from $\Delta_{FTM}$'))
+ax2.set_title('Shaft accel vs time')
 
 fig3, ax3 = plt.subplots()
 ax3.plot(rawPhase, d2Angle_dt2, marker='.', linestyle='none')
 ax3.plot(rawPhase, inputCapAccel, marker='.', linestyle='none')
 ax3.set_xlabel('rotor angle (rad)')
 ax3.set_ylabel('accel (rad/s^2)')
-ax3.legend((r'$\theta(t)$', r'$\Delta_{FTM}(t)$'))
+ax3.legend(('from motor.phase', r'from $\Delta_{FTM}$'))
+ax3.set_title('Shaft accel vs rotor angle')
 
-"""
-# calculate smoothed data
-#alpha_smth = smooth(alpha,window_len=11,window='hanning')
 
 # the first and last several points deviate from the majority behavior. Throw them away
-discardPts = 30
+#discardPts = 30
 #plt.plot(rawAngle[discardPts:-discardPts],alpha_smth[discardPts:-discardPts],marker='.',linestyle='none')
 
-"""
-#downsample the smoothed data to create a look-up-table based on the 100-point encoder count
-[tickCount_ang, alpha_ang_avg, alpha_ang_std] = resample(rawPhase, d2Angle_dt2, N_enc)
-[tickCount_spd, alpha_spd_avg, alpha_spd_std] = resample(rawPhase, inputCapAccel, N_enc)
+
+# Downsample the acceleration data to create an n_sample look-up-table
+n_sample = 100
+[tickCount_ang, alpha_ang_avg, alpha_ang_std] = resample(rawPhase, d2Angle_dt2, n_sample)
+[tickCount_spd, alpha_spd_avg, alpha_spd_std] = resample(rawPhase, inputCapAccel, n_sample)
+
+# calculate smoothed data
+alpha_smth = smooth(alpha_ang_avg, window_len=11, window='hanning')
 
 fig4, ax4 = plt.subplots()
-ax4.plot(tickCount_ang, alpha_ang_avg)
-ax4.plot(tickCount_spd, alpha_spd_avg)
+ax4.errorbar(tickCount_ang, alpha_ang_avg, yerr=alpha_ang_std, marker='.', capsize=3, linestyle='none', label='from motor.phase')
+#ax4.errorbar(tickCount_spd, alpha_spd_avg, yerr=alpha_spd_std, marker='.', capsize=3, linestyle='none', label=r'from $\Delta_{FTM}$')
+ax4.plot(tickCount_ang, alpha_smth, label='smoothed, from motor.phase', color='#d62728')
 ax4.set_xlabel('rotor angle (rad)')
 ax4.set_ylabel('accel (rad/s^2)')
-ax4.legend((r'$\theta(t)$', r'$\Delta_{FTM}(t)$'))
+ax4.legend()
+ax4.set_title(f'Shaft accel vs rotor angle, down-sampled to {n_sample} elements')
 
 #convert angular acceleration (alpha) to torque, and then current
-torque = moment*alpha_ang_avg
+torque = moment*alpha_smth
 current_corr = torque/torqueConst #in Amps
 
 #currently, the output current is scaled such that full-scale = 1.65 A
@@ -223,24 +241,5 @@ current_corr = current_corr/1.65 #as a float
 
 #convert current (as a float) to a frac16_t
 current_Q_F16 = 0x8000*current_corr
-"""
-np.savetxt(r'D:\Documents\Projects\SR544\Data\torqueCorr_LUT.txt',current_Q_F16,newline=',\r\n',fmt='%d')
 
-
-plt.figure(1)
-plt.plot(rawAngle,alpha_smth,marker='o',linestyle='none')
-plt.errorbar(tickCount,alpha_avg, yerr=alpha_std,color='orange', marker='.',linestyle='None')
-plt.xlabel('motor angle (rad)')
-plt.ylabel('alpha (rad/s^2)')
-
-fig, ax1 = plt.subplots()
-ax1.set_xlabel('tick count')
-ax1.set_ylabel('current correction (FRAC16)')
-ax1.plot(current_Q_F16)
-
-ax2 = ax1.twinx()
-ax2.set_ylabel('current correction/full scale')
-ax2.plot(current_Q_F16/(2**15))
-"""
-
-fileWriter.saveDataWithHeader(os.path.basename(__file__), filename, current_Q_F16.astype(int), 'frac16_t', 0, 'currentQcomp')
+#fileWriter.saveDataWithHeader(os.path.basename(__file__), filename, current_Q_F16.astype(int), 'frac16_t', 0, 'currentQcomp')
